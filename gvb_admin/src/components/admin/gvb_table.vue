30dfe86de83ef5313eac8da0b44c0a4b453a0170
GuoChengH <2834592128@qq.com>
2024-07-23 18:13:53 +0800

    refactor: 分割组件可以通过slot配置等等

---
+++ gvb_admin/src/components/admin/gvb_table.vue
@@ -0,0 +1,217 @@
+<template>
+    <div class="gvb_container">
+        <div class="gvb_search">
+            <a-input-search 
+                :placeholder="props.likeTitle" 
+                v-model:value="page.key" 
+                style="width: 200px" 
+                @search="onSearch" 
+            />
+            <slot name="filters"></slot>
+            <div class="gvb_refresh">
+                    <a-button title="刷新" @click="refresh"><i class="fa fa-refresh"></i></a-button>
+            </div>
+        </div>
+        <div class="gvb_divider"></div>
+        <div class="gvb_actions">
+            <slot name="add">
+                <a-button type="primary" @click="addModal" v-if="props.isAdd">添加</a-button>
+            </slot>
+            <slot name="batchRemove">
+                <a-button type="primary" danger @click="removeBatch"
+                    v-if="isDelete && data.selectedRowKeys.length">批量删除</a-button>
+            </slot>
+        </div>
+        <div class="gvb_tables">
+            <a-spin :spinning="data.spinning" tip="Loading..." :delay="500">
+                <a-table :columns="props.columns" :data-source="data.list" :row-key="record => record.id"
+                    :pagination="false"
+                    :row-selection="{ selectedRowKeys: data.selectedRowKeys, onChange: onSelectChange }">
+                    <template #bodyCell="{ column, record }">
+                        <slot name="cell" v-bind="{ column, record }">
+                            <template v-if="column.key === 'created_at'">
+                                {{ getFormatDate(record.created_at) }}
+                            </template>
+                            <template v-if="column.key === 'action'">
+                                <slot name="edit" v-bind="{ column, record }">
+                                    <a-button type="primary" v-if="props.isEdit">编辑</a-button>
+                                </slot>
+                                <slot name="delete" v-bind="{ column, record }">
+                                    <a-popconfirm title="是否确定删除?" ok-text="确认" cancel-text="取消"
+                                        @confirm="userRemove(record.id)" v-if="props.isDelete">
+                                        <a-button class="gvb_table_action delete" type="primary" danger>删除</a-button>
+                                    </a-popconfirm>
+                                </slot>
+
+                            </template>
+                        </slot>
+
+                    </template>
+                </a-table>
+            </a-spin>
+        </div>
+        <div class="gvb_pages">
+            <a-pagination show-less-items v-model:current="page.page" @change="pageChange"
+                v-model:page-size="page.limit" :total="data.count" :show-total="total => ` 共 ${total} 人`" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+import { getFormatDate } from '@/utils/data';
+import { userCreateApi, useRemoveBatchApi, userListApi, updateUserNickNameApi } from '@/api/user_api';
+import { message } from 'ant-design-vue';
+import { baseListApi } from '@/api/base_api';
+
+const emits = defineEmits(["delete", "edit"])
+const props = defineProps({
+    columns: {
+        type: Array,
+    },
+    baseURL: {
+        type: String,
+    },
+    isAdd: {
+        type: Boolean,
+        default: true
+    },
+    isEdit: {
+        type: Boolean,
+        default: true
+
+    },
+    isDelete: {
+        type: Boolean,
+        default: true
+    },
+    filters:{
+        type: Array,
+    },
+    pageSize:{
+        type: Number,
+        default: 7
+    },
+    likeTitle:{
+        type: String,
+        default: "模糊搜索"
+    },
+})
+const page = reactive({
+    page: 1,
+    limit: props.pageSize,
+    key: "",
+});
+const data = reactive({
+    list: [],
+    selectedRowKeys: [],
+    count: 0,
+    spinning: true,
+});
+
+function onSelectChange(selectedRowkeys) {
+    data.selectedRowKeys = selectedRowkeys;
+}
+
+async function removeBatch() {
+    emits("delete", [data.selectedRowKeys]);
+}
+
+function pageChange(_page,limit) {
+    getData(page);
+}
+
+async function userRemove(user_id) {
+    emits("delete", [user_id]);
+}
+async function getData(params) {
+    data.spinning = true;
+    let res = await baseListApi(props.baseURL, params);
+    data.list = res.data.list;
+    data.count = res.data.count;
+    data.spinning = false;
+}
+
+function onSearch(){
+    page.key = page.key.trim();
+    page.page = 1;
+    getData(page);
+}
+function refresh() {
+    getData(page);
+    message.success('刷新成功');
+}
+
+function addModal() {
+    data.modalVidible = true;
+}
+function ExportList(params){
+    page.page = 1;
+    Object.assign(page,params)
+    getData(page)   
+
+}
+defineExpose({
+    ExportList,
+})
+getData(page);
+</script>
+
+<style lang="scss">
+.gvb_container {
+    background-color: white;
+
+    .gvb_search {
+        padding: 10px;
+        border-bottom: var(--card_bg);
+
+        position: relative;
+
+        .gvb_refresh {
+            position: absolute;
+            right: 10px;
+            top: 10px;
+
+            i {
+                color: var(--text);
+            }
+        }
+
+    }
+
+    .gvb_divider {
+        width: 100%;
+        height: 1px;
+        background-color: var(--divider-color, #dcdcdc);
+        margin: 10px 0;
+    }
+
+    .gvb_actions {
+        padding: 10px;
+
+        .ant-btn {
+            margin-right: 10px;
+        }
+    }
+
+    .gvb_tables {
+        padding: 10px;
+    }
+
+    .gvb_pages {
+        display: flex;
+        justify-content: center;
+        padding: 10px;
+    }
+
+    .gvb_table_avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 1cap;
+    }
+
+    .gvb_table_action.update {
+        margin-right: 10px;
+    }
+}
+</style>
--- gvb_admin/src/views/admin/user_mgt/user_list.vue
+++ gvb_admin/src/views/admin/user_mgt/user_list.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="gvb_container">
+    <div>
         <a-modal v-model:visible="data.modalVidible" title="添加用户" @ok="handleOk">
             <a-form :model="formState" name="basic" :label-col="{ span: 4 }" :wrapper-col="{ span: 16 }" ref="formRef"
                 autocomplete="off">
@@ -31,7 +31,6 @@
         <a-modal v-model:visible="data.modalUpdateVidible" title="编辑用户" @ok="update">
             <a-form :model="formUpdateState" name="basic" :label-col="{ span: 4 }" :wrapper-col="{ span: 16 }"
                 ref="formRef" autocomplete="off">
-
                 <a-form-item label="昵称" has-feedback name="nick_name"
                     :rules="[{ required: true, message: '请输入昵称', trigger: 'blur' }]">
                     <a-input v-model:value="formUpdateState.nick_name" placeholder="昵称" />
@@ -44,69 +43,57 @@
                 </a-form-item>
             </a-form>
         </a-modal>
-        <div class="gvb_search">
-            <a-input-search placeholder="搜索用户昵称" v-model:value="page.key" style="width: 200px" @search="onSearch"/>
-            <div class="gvb_refresh">   
-                <a-button title="刷新" @click="refresh"><i class="fa fa-refresh"></i></a-button>
-            </div>
-        </div>
-        <div class="gvb_divider"></div>
-        <div class="gvb_actions">
-            <a-button type="primary" @click="addModal">添加</a-button>
-            <a-button type="primary" danger @click="removeBatch" v-if="data.selectedRowKeys.length">批量删除</a-button>
 
-        </div>
-        <div class="gvb_tables">
-            <a-spin :spinning="data.spinning" tip="Loading..." delay="500">
-                <a-table :columns="data.columns" :data-source="data.list" :row-key="record => record.id"
-                    :pagination="false"
-                    :row-selection="{ selectedRowKeys: data.selectedRowKeys, onChange: onSelectChange }">
-                    <template #bodyCell="{ column, record }">
-                        <template v-if="column.key === 'avatar'">
-                            <img class="gvb_table_avatar" :src="record.avatar" alt="" />
-                        </template>
-                        <template v-if="column.key === 'created_at'">
-                            {{ getFormatDate(record.created_at) }}
-                        </template>
-                        <template v-if="column.key === 'action'">
-                            <a-button class="gvb_table_action update" type="primary"
-                                @click="updateModal(record)">编辑</a-button>
-                            <a-popconfirm title="是否确定删除?" ok-text="确认" cancel-text="取消"
-                                @confirm="userRemove(record.id)">
-                                <a-button class="gvb_table_action delete" type="primary" danger>删除</a-button>
-                            </a-popconfirm>
-
-                        </template>
-                    </template>
-                </a-table>
-            </a-spin>
-
-        </div>
-        <div class="gvb_pages">
-            <a-pagination show-less-items v-model:current="page.page" @change="pageChange"
-                v-model:page-size="page.limit" :total="data.count" :show-total="total => ` 共 ${total} 人`" />
-        </div>
+        <GVBTable 
+            @delete="userDelete" 
+            @edit="updateModal" 
+            :columns="data.columns" 
+            baseURL="/api/users" 
+            ref="gvbTable"
+            :page-size="7"
+            :like-title="'搜索用户昵称'"
+        >
+            <template #add>
+                <a-button type="primary" @click="data.modalVidible = true">添加</a-button>
+            </template>
+            <template #edit="{ record }">
+                <a-button class="gvb_table_action update" type="primary" @click="updateModal(record)">编辑</a-button>
+            </template>
+            <template #cell="{ column, record }">
+                <template v-if="column.key === 'avatar'">
+                    <img class="gvb_table_avatar" :src="record.avatar" alt="" />
+                </template>
+            </template>
+            <template #filters>
+                <a-select class="gvb_select" allowClear v-model:value="filter" @change="onFilters"
+                    style="width: 200px" :options="options" placeholder="选择权限">
+                </a-select>
+                <!-- <div class="gvb_refresh">
+                    <a-button title="刷新" @click="refresh"><i class="fa fa-refresh"></i></a-button>
+                </div> -->
+            </template>
+        </GVBTable>
     </div>
 </template>
 
 <script setup>
 import { reactive, ref } from 'vue'
-import { getFormatDate } from '@/utils/data';
-import { userCreateApi, useRemoveBatchApi, userListApi, updateUserNickNameApi } from '@/api/user_api';
+import { userCreateApi, updateUserNickNameApi, userListApi } from '@/api/user_api';
 import { message } from 'ant-design-vue';
+import GVBTable from '@/components/admin/gvb_table.vue'
 
 const formRef = ref(null);
-const formUpdateState = reactive({
-    nick_name: "",
-    role: undefined,
-    user_id: 0
-})
-const page = reactive({
-    page: 1,
-    limit: 7,
-    key:"",
-})
+const filter = ref(undefined)
+const gvbTable = ref(null);
+const options = [
+    { label: '管理员', value: 1 },
+    { label: '用户', value: 2 },
+    { label: '游客', value: 3 }
+];
+
 const data = reactive({
+    modalVidible: false, // 添加用户modal
+    modalUpdateVidible: false, // 编辑用户modal
     columns: [
         { title: 'id', key: 'id', dataIndex: 'id' },
         { title: '昵称', key: 'nick_name', dataIndex: 'nick_name' },
@@ -119,86 +106,37 @@ const data = reactive({
         { title: '创建时间', key: 'created_at', dataIndex: 'created_at' },
         { title: '操作', key: 'action', dataIndex: 'action' },
     ],
-    list: [
-        {
-            "id": 1,
-            "created_at": "2024-04-06T10:48:43.354+08:00",
-            "nick_name": "xiaohei",
-            "user_name": "xiaohei",
-            "avatar": "/uploads/avatar/default.jpg",
-            "email": "x****@qq.com",
-            "tel": "",
-            "addr": "本地",
-            "token": "",
-            "ip": "127.0.0.1",
-            "role": "用户",
-            "sign_status": "邮箱"
-        },
-    ],
-    selectedRowKeys: [],
-    count: 0,
-    modalVidible: false,
-    modalUpdateVidible: false,
-    spinning:true,
-})
-const options = [{
-    label: '管理员',
-    value: 1
-},
-{
-    label: '普通用户',
-    value: 2
-},
-{
-    label: '游客',
-    value: 3
-}]
+});
+
+const formUpdateState = reactive({
+    nick_name: "",
+    role: undefined,
+    user_id: 0
+});
+
 const formState = reactive({
     nick_name: "",
     user_name: "",
     password: "",
     re_password: "",
     role: 1
-})
-function onSelectChange(selectedRowkeys) {
-    data.selectedRowKeys = selectedRowkeys;
-}
-async function removeBatch() {
-    // data.selectedRowKeys
-    let res = await useRemoveBatchApi(data.selectedRowKeys)
-    if (res.code) {
-        message.error(res.msg);
-        return;
-    }
-    message.success(res.msg);
-    getData()
+});
 
+function onFilters(){
+    console.log(filter.value);
+    gvbTable.value.ExportList({role:filter.value})
 }
-function pageChange() {
-    getData();
+function userDelete(userIdList) {
+    console.log(userIdList);
 }
-async function userRemove(user_id) {
-    let res = await useRemoveBatchApi([user_id])
-    if (res.code) {
-        message.error(res.msg);
-        return;
-    }
-    message.success(res.msg);
-    getData()
-}
-async function getData() {
-    data.spinning = true
-    let res = await userListApi(page)
-    data.list = res.data.list
-    data.count = res.data.count
-    data.spinning = false
-    console.log(res.data)
-}
-function refresh() {
-    getData()
-    message.success('刷新成功')
 
+function updateModal(record) {
+    data.modalUpdateVidible = true;
+    formUpdateState.user_id = record.id;
+    formUpdateState.nick_name = record.nick_name;
+    formUpdateState.role = record.role;
 }
+
 async function handleOk() {
     try {
         await formRef.value?.validate();
@@ -225,92 +163,25 @@ let validatePassword = async (_rule, value) => {
     } else {
         return Promise.resolve();
     }
-}
-function addModal() {
-    data.modalVidible = true;
+};
 
-}
-
-function updateModal(record) {
-    data.modalUpdateVidible = true;
-    formUpdateState.user_id = record.id;
-    formUpdateState.nick_name = record.nick_name;
-    formUpdateState.role = record.role;
-}
 async function update() {
     console.log(formUpdateState);
-    let res = await updateUserNickNameApi(formUpdateState)
+
+    let res = await updateUserNickNameApi(formUpdateState);
     if (res.code) {
         message.error(res.msg);
         return;
     }
     message.success(res.msg);
-    getData()
     data.modalUpdateVidible = false;
+    getData();
 }
-function onSearch(){
-    page.key = page.key.trim()
-    page.page = 1
-    getData()
+async function getData() {
+    // 调用 GVBTable 组件中的 getData 方法
+    const tableComponent = ref(null);
+    tableComponent.value.getData();
 }
-getData()
 </script>
 
-<style lang="scss">
-.gvb_container {
-    background-color: white;
-
-    .gvb_search {
-        padding: 10px;
-        border-bottom: var(--card_bg);
-
-        position: relative;
-
-        .gvb_refresh {
-            position: absolute;
-            right: 10px;
-            top: 10px;
-
-            i {
-                color: var(--text);
-            }
-        }
-
-    }
-
-    .gvb_divider {
-        width: 100%;
-        height: 1px;
-        background-color: var(--divider-color, #dcdcdc);
-        margin: 10px 0;
-    }
-
-    .gvb_actions {
-        padding: 10px;
-
-        .ant-btn {
-            margin-right: 10px;
-        }
-    }
-
-    .gvb_tables {
-        padding: 10px;
-    }
-
-    .gvb_pages {
-        display: flex;
-        justify-content: center;
-        padding: 10px;
-    }
-
-    .gvb_table_avatar {
-        width: 40px;
-        height: 40px;
-        border-radius: 1cap;
-    }
-
-    .gvb_table_action.update {
-        margin-right: 10px;
-    }
-}
-</style>
+<style lang="scss"></style>
