fb6ef3c0c22e229a5581f9571e7855fd582c47a6
GuoChengH <2834592128@qq.com>
2024-08-11 23:06:03 +0800

    feat: 新增全局的ctrl+s保存文章信息

--- gvb_admin/src/views/admin/article_mgr/add_article.vue
+++ gvb_admin/src/views/admin/article_mgr/add_article.vue
@@ -1,14 +1,13 @@
 <template>
-    <md-editor v-model="text" @onUploadImg="onUploadImg" />
+    <md-editor v-model="data.content" @onUploadImg="onUploadImg" @on-save="onSave" />
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { onUnmounted, reactive, ref } from 'vue';
 import { MdEditor } from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 import { uploadImageApi } from '@/api/image_api';
 
-const text = ref(''); // 初始化空字符串
 const onUploadImg = async (files, callback) => {
     const res = await Promise.all(
         files.map((file) => {
@@ -25,4 +24,27 @@ const onUploadImg = async (files, callback) => {
     }).filter(url => url !== ''); // 过滤掉空字符串
     callback(fileUrls);
 };
+const data = reactive({
+    content: "hello"
+})
+
+
+function ctrlSave(e) {
+    if (e.ctrlKey && e.key === 's') {
+        showDrawer();
+        e.preventDefault();
+        onSave(text.value, '');
+    }
+}
+function showDrawer() {
+    visible.value = true
+}
+window.addEventListener('keydown', ctrlSave);
+onUnmounted(() => {
+    window.removeEventListener('keydown', ctrlSave);
+});
+// ctrl + s 保存 md是md原文，h是转换后的
+const onSave = (md, h) => {
+    showDrawer();
+}
 </script>
