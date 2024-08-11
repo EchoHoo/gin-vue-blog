dbdadf5b015fe2a97ca9d6907d4174e4673e1fce
GuoChengH <2834592128@qq.com>
2024-08-11 22:48:44 +0800

    feat: 新增在组件上上传图片

--- gvb_admin/src/api/image_api.js
+++ gvb_admin/src/api/image_api.js
@@ -7,3 +7,19 @@ export function imageRenameApi(data) {
 export function imageNameListApi(){
     return Service.get("/api/image_names")
 }
+
+
+export function uploadImageApi(file){
+    return new Promise( (resolve,reject)=>{
+        const form = new FormData();
+        form.append('images', file);
+        Service.post("/api/images",form,{
+            headers:{
+                'Content-Type': 'multipart/form-data'
+            }
+        }) .then((res) => resolve(res))
+            .catch((error) => reject(error));
+})
+       
+
+}
--- gvb_admin/src/views/admin/article_mgr/add_article.vue
+++ gvb_admin/src/views/admin/article_mgr/add_article.vue
@@ -1,11 +1,28 @@
 <template>
-    <MdEditor v-model="text" />
+    <md-editor v-model="text" @onUploadImg="onUploadImg" />
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
 import { MdEditor } from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
+import { uploadImageApi } from '@/api/image_api';
 
-
+const text = ref(''); // 初始化空字符串
+const onUploadImg = async (files, callback) => {
+    const res = await Promise.all(
+        files.map((file) => {
+            return uploadImageApi(file);
+        })
+    );
+    // 处理返回结果，提取文件路径
+    const fileUrls = res.map((item) => {
+        const data = item.data;
+        if (Array.isArray(data) && data.length > 0) {
+            return data[0].file_name; // 假设每次只上传一个文件
+        }
+        return '';
+    }).filter(url => url !== ''); // 过滤掉空字符串
+    callback(fileUrls);
+};
 </script>
