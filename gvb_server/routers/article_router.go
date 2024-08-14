2176e7d9aeb80bbc2763f79bc6399281a472a34e
GuoChengH <2834592128@qq.com>
2024-08-14 12:00:41 +0800

    fix: 删除了之前的冗余代码

--- gvb_server/routers/article_router.go
+++ gvb_server/routers/article_router.go
@@ -9,7 +9,7 @@ func (router RouterGroup) ArticleRouter() {
 	app := api.ApiGroupApp.ArticleApi
 	router.POST("articles", middleware.JwtAuth(), app.ArticleCreateView)
 	router.DELETE("articles", app.ArticleRemoveView)
-	router.GET("articles", app.ArticleListView)
+	router.GET("articles", app.ArticleListView) // 可以传入token 和 is_user 来控制用户发布的文章
 	router.PUT("articles", app.ArticleUpdateView)
 
 	router.GET("article/detail", app.ArticleDetailByTitleView)
@@ -25,5 +25,4 @@ func (router RouterGroup) ArticleRouter() {
 	router.GET("categorys", app.ArticleCategoryListView)          //全文搜索
 	router.GET("article/content/:id", app.ArticleContentByIDView) //根据ID得到文章内容
 
-	router.GET("user_articles", middleware.JwtAuth(), app.UserArticleListView) // 获取用户的文章列表
 }
