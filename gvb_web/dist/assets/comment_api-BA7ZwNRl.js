9efd19fb04029e0241b03e2bae25d39a1ee4ca00
GuoChengH <2834592128@qq.com>
2024-09-01 19:54:38 +0800

    feat: 重新生成main和web下的静态文件

---
+++ gvb_web/dist/assets/comment_api-BA7ZwNRl.js
@@ -0,0 +1,1 @@
+import{S as t}from"./index-Sy54AUqK.js";function m(e){return t.delete("/api/comments/"+e)}function n(e){return t.post("/api/comments",e)}function o(e){return t.get("/api/comments?article_id="+e)}export{m as a,n as c,o as g};
